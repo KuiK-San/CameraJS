1b227ee77fd728e3514b8379e713ea040d059602
Guilherme Casagrande <thegui4000@gmail.com>
2023-09-22 11:28:54 -0300

    fix: não havia add o script

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-var video = document.querySelector('#camera')
+var video = document.querySelector('video')
 const specs = {video:{width:320}}
 
-const startButton = document.getElementById('startCamera');
-
-startButton.addEventListener('click', async () => {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia(specs);
-        video.srcObject = stream;
-        video.play();
-    } catch (error) {
-        console.error('Erro ao iniciar a câmera:', error);
-    }
-});
+navigator.mediaDevices.getUserMedia(specs)
+.then( stream => {
+    video.srcObject = stream;
+    video.play();
+})
+.catch(error => {
+    console.log(error)
+})
 
 document.querySelector('button').addEventListener('click', () =>{
-    var canvas = document.querySelector('#imagem');
+    var canvas = document.querySelector('canvas');
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
     var context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0);
-    var link = document.createElement('a');
-    link.download = 'foto.png';
-    link.href = canvas.toDataURL();
-    link.textContent = 'Clique para baixar a imagem';
-    document.body.appendChild(link);
-})
